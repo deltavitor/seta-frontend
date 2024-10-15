2eff4c44c6ac6d01d17d39ba6df39ec232a94af6
deltavitor <cvitor1618@gmail.com>
2024-10-15 08:23:31 -0300

    Update App component with new states and update Map component with the new layer controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,59 @@
 import "@fontsource-variable/inter";
 import "./App.scss";
-import { Map, NotificationFilter, StartupCard } from "./modules";
+import { Map, MapLayerFilter, NotificationFilter, StartupCard } from "./modules";
 import { useFindNotificationLocations } from "./hooks";
 import { useEffect, useState } from "react";
 import { Button } from "./modules/core";
-import { Filter } from "lucide-react";
+import { Filter, Layers } from "lucide-react";
 import { getNotificationType } from "./utils";
-import type { Notification, NotificationLocation, NotificationTypeFilter } from "./types";
+import type { MapLayerFilter as MapLayerFilterType, Notification, NotificationLocation, NotificationTypeFilter } from "./types";
 import NotificationPane from "./modules/notification-pane/NotificationPane";
 
 function App() {
 
     const { data: notificationLocationData, isLoading } = useFindNotificationLocations();
+
     const [notificationLocations, setNotificationLocations] = useState<Array<NotificationLocation>>();
+    const [validNotificationLocations, setValidNotificationLocations] = useState<Array<NotificationLocation>>();
     const [filteredNotificationLocations, setFilteredNotificationLocations] = useState<Array<NotificationLocation>>();
-    const [notificationFiltersAreVisible, setNotificationFiltersAreVisible] = useState(false);
     const [notificationLocation, setNotificationLocation] = useState<NotificationLocation>();
     const [numeroNotificacao, setNumeroNotificacao] = useState<Notification["numeroNotificacao"]>("0");
 
-    const handleNotificationsFilterChange = (filters: NotificationTypeFilter) => {
-        setFilteredNotificationLocations(notificationLocations?.map(notificationLocation => {
+    // Visibility / UI states
+    const [controlPanelFiltersVisibility, setControlPanelFiltersVisibility] = useState({
+        notificationFilters: false,
+        mapLayerFilters: false,
+    });
+
+    const [notificationTypeFilter, setNotificationTypeFilter] = useState<NotificationTypeFilter>({
+        labConfirmed: true,
+        labDiscarded: true,
+        clinicalConfirmed: true,
+        clinicalDiscarded: true,
+        underInvestigation: true,
+    });
+    const [mapLayerFilter, setMapLayerFilter] = useState<MapLayerFilterType>({
+        notifications: true,
+        heatmap: false,
+    });
+
+    const toggleControlPanelFilter = (filterKey: keyof typeof controlPanelFiltersVisibility) => {
+        setControlPanelFiltersVisibility(prevState =>
+            Object.assign(
+                {},
+                ...Object.keys(prevState).map(key => ({ [key]: key === filterKey ? !prevState[filterKey] : false }))
+            )
+        );
+    };
+
+    const isValidNotificationLocation = (notificationLocation: NotificationLocation) => {
+        return notificationLocation.locationType !== "APPROXIMATE";
+    };
+
+    useEffect(() => {
+        setFilteredNotificationLocations(validNotificationLocations?.map(notificationLocation => {
             const filteredNotifications = notificationLocation.notifications.filter(notification => {
-                return filters[getNotificationType(notification)];
+                return notificationTypeFilter[getNotificationType(notification)];
             });
 
             return {
@@ -29,11 +61,15 @@ function App() {
                 notifications: filteredNotifications
             };
         }).filter(notificationLocation => notificationLocation.notifications.length > 0));
-    };
+    }, [notificationTypeFilter]);
 
     useEffect(() => {
         setNotificationLocations(notificationLocationData);
     }, [notificationLocationData]);
+
+    useEffect(() => {
+        setValidNotificationLocations(notificationLocations?.filter(isValidNotificationLocation));
+    }, [notificationLocations]);
 
     // TODO improve this
     return (
@@ -42,14 +78,38 @@ function App() {
             { !isLoading && notificationLocations?.length == 0 && <StartupCard /> }
             { notificationLocations && notificationLocations.length > 0 && !isLoading &&
                 <div>
-                    <div style={{position: "absolute", zIndex: 1000, left: 20, bottom: 20}}>
-                        <NotificationFilter hidden={!notificationFiltersAreVisible} handleFilterChange={(filters) => handleNotificationsFilterChange(filters)}></NotificationFilter>
-                        <Button kind={"primary"} onClick={() => setNotificationFiltersAreVisible(!notificationFiltersAreVisible)}>
-                            Filtros <Filter size={20} />
-                        </Button>
+                    <div style={{
+                        position: "absolute",
+                        zIndex: 1000,
+                        left: 20,
+                        bottom: 20,
+                        display: "inline-flex",
+                        gap: "2rem"
+                    }}>
+                        <div style={{position: "relative", display: "inline-block"}}>
+                            <MapLayerFilter hidden={!controlPanelFiltersVisibility.mapLayerFilters}
+                                            filters={mapLayerFilter} setMapLayerFilter={setMapLayerFilter}/>
+                            <Button kind={"primary"} onClick={() => toggleControlPanelFilter("mapLayerFilters")}>
+                                Camadas <Layers size={20}/>
+                            </Button>
+                        </div>
+                        <div style={{position: "relative", display: "inline-block"}}>
+                            <NotificationFilter hidden={!controlPanelFiltersVisibility.notificationFilters}
+                                                filters={notificationTypeFilter} setNotificationTypeFilter={setNotificationTypeFilter}/>
+                            <Button kind={"primary"} onClick={() => toggleControlPanelFilter("notificationFilters")}>
+                                Filtros <Filter size={20}/>
+                            </Button>
+                        </div>
                     </div>
-                    <NotificationPane numeroNotificacao={numeroNotificacao} setNumeroNotificacao={setNumeroNotificacao} notificationLocation={notificationLocation} />
-                    <Map notificationLocations={filteredNotificationLocations || notificationLocations} setNumeroNotificacao={setNumeroNotificacao} notificationLocation={notificationLocation} setNotificationLocation={setNotificationLocation}></Map>
+                    <NotificationPane numeroNotificacao={numeroNotificacao} setNumeroNotificacao={setNumeroNotificacao}
+                                      notificationLocation={notificationLocation}/>
+                    <Map
+                        notificationLocations={filteredNotificationLocations || validNotificationLocations || notificationLocations}
+                        setNumeroNotificacao={setNumeroNotificacao}
+                        notificationLocation={notificationLocation}
+                        setNotificationLocation={setNotificationLocation}
+                        mapLayerFilter={mapLayerFilter}
+                    />
                 </div>
             }
         </div>
--- src/modules/map/Map.tsx
+++ src/modules/map/Map.tsx
@@ -1,21 +1,29 @@
 import colors from "../../styles/colors.module.scss";
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { LayerGroup, LayersControl, MapContainer, Marker, TileLayer } from "react-leaflet";
 import { Marker as SetaMarker, type MarkerProps } from "../core";
-import type { Notification, NotificationLocation } from "../../types";
+import type { MapLayerFilter, Notification, NotificationLocation } from "../../types";
 import { DivIcon } from "leaflet";
-import React, { type ReactNode } from "react";
+import React, { type ReactNode, useEffect } from "react";
 import { renderToString } from "react-dom/server";
 import { ClassificacoesFinais, CriteriosConfirmacao } from "../../consts";
 import { MapPin } from "lucide-react";
+import { HeatmapLayerFactory } from "@vgrid/react-leaflet-heatmap-layer";
 
 type MapProps = {
     notificationLocations: Array<NotificationLocation>;
     setNumeroNotificacao: React.Dispatch<React.SetStateAction<Notification["numeroNotificacao"]>>;
     notificationLocation?: NotificationLocation;
     setNotificationLocation: React.Dispatch<React.SetStateAction<NotificationLocation | undefined>>;
+    mapLayerFilter: MapLayerFilter;
 };
 
+const HeatmapLayer = HeatmapLayerFactory<[number, number, number]>();
+
 function Map(props: MapProps) {
+
+    const [heatMapPoints, setHeatMapPoints] = React.useState<Array<[number, number, number]>>([]);
+
+    const layerControlInputs = document.querySelectorAll<HTMLInputElement>(".leaflet-control-layers-selector");
 
     const centerLatitude = props.notificationLocations[0]?.latitude;
     const centerLongitude = props.notificationLocations[0]?.longitude;
@@ -29,9 +37,11 @@ function Map(props: MapProps) {
         const { latitude, longitude } = notificationLocation;
         const isSelectedNotificationLocation = notificationLocation.numeroNotificationLocation === selectedNotificationLocation?.numeroNotificationLocation;
 
-        const markerSize = isSelectedNotificationLocation ? 42 : 24;
+        const markerSize = isSelectedNotificationLocation ? 36 : 20;
 
         const firstNotification = notificationLocation.notifications[0];
+        if (!firstNotification) return null;
+
         const props: MarkerProps = {
             markerSize: markerSize,
             shape: firstNotification.criterioConfirmacao === CriteriosConfirmacao.ConfirmedThroughLabAnalysis ? "diamond" : "circle",
@@ -77,12 +87,50 @@ function Map(props: MapProps) {
             props.setNumeroNotificacao("-1");
     };
 
+    useEffect(() => {
+        setHeatMapPoints(props.notificationLocations.map((notificationLocation: NotificationLocation) => {
+            return [
+                notificationLocation.latitude,
+                notificationLocation.longitude,
+                notificationLocation.notifications.length
+            ];
+        }));
+    }, [props.notificationLocations]);
+
+    useEffect(() => {
+        layerControlInputs.forEach(input => {
+            input.nextElementSibling && input.nextElementSibling.textContent?.trim() === "notifications" ? input.click() : null;
+        });
+    }, [props.mapLayerFilter.notifications]);
+
+    useEffect(() => {
+        layerControlInputs.forEach(input => {
+            input.nextElementSibling && input.nextElementSibling.textContent?.trim() === "heatmap" ? input.click() : null;
+        });
+    }, [props.mapLayerFilter.heatmap]);
+
     return (
-        <MapContainer center={[centerLatitude, centerLongitude]} zoom={15} zoomControl={false} attributionControl={false} >
-            <TileLayer url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png" />
-            {props.notificationLocations.map((notificationLocation, index) => {
-                return getNotificationLocationMarker(index, notificationLocation, props.notificationLocation)
-            })}
+        <MapContainer center={[centerLatitude, centerLongitude]} zoom={14} zoomControl={false} attributionControl={false}>
+            <TileLayer
+                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+            />
+            <LayersControl position={"topleft"}>
+                <LayersControl.Overlay name={"notifications"} checked={true}>
+                    <LayerGroup>
+                        {props.notificationLocations.map((notificationLocation, index) => {
+                            return getNotificationLocationMarker(index, notificationLocation, props.notificationLocation)
+                        })}
+                    </LayerGroup>
+                </LayersControl.Overlay>
+                <LayersControl.Overlay name={"heatmap"} checked={false}>
+                    <HeatmapLayer
+                        points={heatMapPoints}
+                        latitudeExtractor={m => m[0]}
+                        longitudeExtractor={m => m[1]}
+                        intensityExtractor={m => m[2] + ((m[2] - 1) * 0.1)}
+                    />
+                </LayersControl.Overlay>
+            </LayersControl>
         </MapContainer>
     );
 }
