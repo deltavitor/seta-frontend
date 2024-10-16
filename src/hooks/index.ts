a4cbf04853eb7015b9f9c98cd0b1ec709af7ac5d
deltavitor <cvitor1618@gmail.com>
2024-10-16 10:25:04 -0300

    Create useInterval hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,13 @@
 import useAddNotifications from "./use-add-notifications";
 import useFindNotificationLocations from "./use-find-notification-locations";
 import useGetNotificationByNumeroNotificacao from "./use-get-notification-by-numero-notificacao";
+import useInterval from "./use-interval";
 import usePredict from "./use-predict";
 
 export {
     useAddNotifications,
     useFindNotificationLocations,
     useGetNotificationByNumeroNotificacao,
+    useInterval,
     usePredict
 };
