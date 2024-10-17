2fea62edb691235c62c218d58642084f6cc34649
deltavitor <cvitor1618@gmail.com>
2024-10-17 09:04:12 -0300

    Update SelectedNotificationContext to include the selectedNotificationDataSintomas and a reference to the related notificationLocations

--- src/contexts/SelectedNotificationContextProvider.tsx
+++ src/contexts/SelectedNotificationContextProvider.tsx
@@ -10,6 +10,10 @@ type SelectedNotificationContext = {
     setSelectedNotificationLocation: Dispatch<SetStateAction<NotificationLocation | undefined>>
     selectedNumeroNotificacao: Notification["numeroNotificacao"],
     setSelectedNumeroNotificacao: Dispatch<SetStateAction<Notification["numeroNotificacao"]>>,
+    selectedNotificationDataSintomas: Notification["dataDiagnosticoSintomaParsed"],
+    setSelectedNotificationDataSintomas: Dispatch<SetStateAction<Notification["dataDiagnosticoSintomaParsed"]>>,
+    relatedNotificationLocations?: Array<NotificationLocation>,
+    setRelatedNotificationLocations: Dispatch<SetStateAction<Array<NotificationLocation> | undefined>>,
 }
 
 const SelectedNotificationContext = createContext<SelectedNotificationContext | undefined>(undefined);
@@ -18,13 +22,19 @@ function SelectedNotificationContextProvider(props: SelectedNotificationContextP
 
     const [selectedNotificationLocation, setSelectedNotificationLocation] = useState<NotificationLocation>();
     const [selectedNumeroNotificacao, setSelectedNumeroNotificacao] = useState<Notification["numeroNotificacao"]>("0");
+    const [selectedNotificationDataSintomas, setSelectedNotificationDataSintomas] = useState<Notification["dataDiagnosticoSintomaParsed"]>();
+    const [relatedNotificationLocations, setRelatedNotificationLocations] = useState<Array<NotificationLocation> | undefined>();
 
     return (
         <SelectedNotificationContext.Provider value={{
             selectedNotificationLocation,
             setSelectedNotificationLocation,
             selectedNumeroNotificacao,
-            setSelectedNumeroNotificacao
+            setSelectedNumeroNotificacao,
+            selectedNotificationDataSintomas,
+            setSelectedNotificationDataSintomas,
+            relatedNotificationLocations,
+            setRelatedNotificationLocations
         }}>
             {props.children}
         </SelectedNotificationContext.Provider>
