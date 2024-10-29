63b7fa60194392e92490e4b5b1af538d9f50d574
deltavitor <cvitor1618@gmail.com>
2024-10-28 21:14:45 -0300

    Add debug statement

--- src/services/notification-location-service.ts
+++ src/services/notification-location-service.ts
@@ -4,6 +4,10 @@ import type { NotificationLocation } from "../types";
 const notificationLocationService = {
 
     findAllNotificationLocations: async function(): Promise<Array<NotificationLocation>> {
+        console.log("The gateway api client URL: ");
+        console.log(import.meta.env.SETA_GATEWAY_URL);
+        console.log("The uri: ");
+        console.log(setaGatewayApiClient.getUri());
         const response = await setaGatewayApiClient.get<Array<NotificationLocation>>("/notification-locations");
         return response.data;
     },
